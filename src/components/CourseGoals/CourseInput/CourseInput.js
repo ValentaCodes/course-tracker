c7ea3abb32d7c3479192d80d9251a9db031e9426
ValentaCodes <valentacodes@gmail.com>
2023-05-09 00:46:51 -0600

    Added synamic styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,23 +5,41 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  //  C
+  const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
+    // reset styling
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true)
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    // NOTE: Best Practice to alter state when depending on previous state
+    setIsValid(isNowValid => !isNowValid)
+
+    // Sets style dynamically avoiding adding empty goals
+    if (enteredValue.trim().length === 0) {
+    setIsValid(false) 
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/* This will inject content into the string allowing dynamic styles */}
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type='text'
+          onChange={goalInputChangeHandler}
+        />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type='submit'>Add Goal</Button>
     </form>
   );
 };
