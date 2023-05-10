eac9265c80331f13f546aa5e990fb197b5a613ef
ValentaCodes <valentacodes@gmail.com>
2023-05-09 23:40:46 -0600

    Added styled-components package

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,27 +3,27 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = props => {
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
-  //  C
-  const [isValid, setIsValid] = useState(true)
 
-  const goalInputChangeHandler = event => {
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
     // reset styling
     if (event.target.value.trim().length > 0) {
-      setIsValid(true)
+      setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     // NOTE: Best Practice to alter state when depending on previous state
-    setIsValid(isNowValid => !isNowValid)
+    setIsValid((isNowValid) => !isNowValid);
 
     // Sets style dynamically avoiding adding empty goals
     if (enteredValue.trim().length === 0) {
-    setIsValid(false) 
+      setIsValid(false);
       return;
     }
     props.onAddGoal(enteredValue);
@@ -31,13 +31,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      {/* This will inject content into the string allowing dynamic styles */}
+      {/* We can use template literals to inject content into the string, allowing for dynamic styling. */}
       <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
-        <input
-          type='text'
-          onChange={goalInputChangeHandler}
-        />
+        <input type='text' onChange={goalInputChangeHandler} />
       </div>
       <Button type='submit'>Add Goal</Button>
     </form>
